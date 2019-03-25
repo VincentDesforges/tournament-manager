41d737b9980704669f37d71becc426f24ea6107d
Vincent Desforges <desforges.vincent@gmail.com>
2019-03-25 17:29:37 +0100

    generated games model and added seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_170121) do
+ActiveRecord::Schema.define(version: 2019_03_25_140257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +35,8 @@ ActiveRecord::Schema.define(version: 2019_03_24_170121) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Clear tables
+puts "=====Starting Seed====="
+puts "Clearing database..."
+User.destroy_all
+Game.destroy_all
+
+# Adding data: ----------------------------------------------
+# Users: ----------------------------------------------------
+puts "Adding Users..."
+User.create!({
+  email:    "vince@vince.com",
+  password: "123456",
+  admin:    true
+})
+
+User.create!({
+  email:    "vince2@vince.com",
+  password: "123456",
+  admin:    false
+})
+
+# Games: ----------------------------------------------------
+puts "Adding Games..."
+
+Game.create!({
+  title: "Chess",
+  description: "2 person strategy game"
+})
+
+Game.create!({
+  title: "Snap",
+  description: "2 person card game involving reflexes"
+})
+
+# Tournaments: ----------------------------------------------
+puts "Adding Tournaments????..."
+# Events: ---------------------------------------------------
+puts "Adding Events?????..."
+# Matches: --------------------------------------------------
+puts "Adding Matches?????..."
+puts "=====End of Seed====="
