2878364e6620918c58719bc2c0f995ed09d997cc
Vincent Desforges <desforges.vincent@gmail.com>
2019-03-27 16:36:30 +0100

    added new and edit pages for tournaments. Also managed routing and permissions

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,16 +1,46 @@
 class TournamentsController < ApplicationController
-  before_action :find_tournament, only: [:show]
-  skip_before_action :authenticate_user!, only: [:show]  
+  before_action :find_tournament, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:show]
+  load_and_authorize_resource 
 
   def show
   end
 
+  def new
+    @tournament = Tournament.new
+  end
+  
+  def create
+    @tournament = Tournament.new(tournament_params)
+    if @tournament.save
+      redirect_to tournament_path(@tournament)
+    else
+      render new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @tournament.update(tournament_params)
+      redirect_to tournament_path(@tournament)
+    else
+      render new
+    end
+  end
+  
+  def destroy
+    @tournament.destroy
+    redirect_to root_path
+  end
+
   private
   
-  # def game_params
-  #   allowableParams = [:name, :max_players, :address, :latitute, :longitude]
-  #   params.require(:tournament).permit(allowableParams)
-  # end
+  def tournament_params
+    allowableParams = [:name, :max_players, :address, :latitute, :longitude]
+    params.require(:tournament).permit(allowableParams)
+  end
 
   def find_tournament
     @tournament = Tournament.find(params[:id])
