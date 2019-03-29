3d56f6bf45a942aa89cedb0cd277c8ade7a276af
Vincent Desforges <desforges.vincent@gmail.com>
2019-03-29 09:14:17 +0100

    added validation to prevent matches against yourself

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,14 @@
 class Match < ApplicationRecord
+  validate :players_cannot_play_themselves
+
   belongs_to :player_1, class_name: 'User'
   belongs_to :player_2, class_name: 'User'
   belongs_to :event
+
+  validates :event, presence: true #Match needs an event
+
+  private
+  def players_cannot_play_themselves
+      errors.add(:player_2_id, 'You cannot play against yourself') if player_1_id == player_2_id
+  end
 end
